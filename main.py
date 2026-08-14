4bdf7335f3f94eef28f19e38a8ca28919e2e353d
Ali <84a.mahdaviniya@gmail.com>
2026-08-15 00:34:26 +0330

    add ping function

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import socket
 import ipaddress
+import subprocess
+
+
+def ping(io):
+    result = subprocess.run(
+        ["ping", "-n", "1", "-w", "1000", str(ip)],
+        stdout=subprocess.DEVNULL
+    )
+
+    return result.returncode == 0
+
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
@@ -15,3 +26,10 @@ print("Local IP: ", local_ip)
 print("Network: ", network)
 print("Netmask: ", network.netmask)
 print("Broadcast: ", network.broadcast_address)
+
+ip = "10.145.18.54"
+
+if ping(ip):
+    print("Online")
+else:
+    print("Offline")
